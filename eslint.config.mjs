a7aeb0eb42e5f70d9805bbe16f964068343c085f
Shakhbulat Gazgireev <shakhbulatgaz@gmail.com>
2026-02-20 15:43:14 +0300

    Add Prettier with shared config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,10 +1,12 @@
 import js from "@eslint/js";
+import prettier from "eslint-config-prettier";
 
 export default [
   {
     ignores: ["onscan.min.js"],
   },
   js.configs.recommended,
+  prettier,
   {
     files: ["onscan.js"],
     languageOptions: {
